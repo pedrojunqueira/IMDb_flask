d20bcda741c3adb0cf5bd8c941ec99c9c13dc293
pedrojunqueira <pedrocj@gmail.com>
2020-02-08 09:41:51 +1030

    added some css and bootstrap 🤪

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from models import clf, vect
 from forms import ReviewForm
 from app_utils import string_list
@@ -9,6 +9,14 @@ app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba243'
 
 label = {0: 'negative', 1: 'positive'}
 
+stored_predictions = [{'id':1,'review':'This movie was good recommend!', 'prediction':'positive','probability':'70%','label':'positive'},
+                      {'id': 2, 'review': 'Hated', 'prediction': 'negative',
+                          'probability': '70%', 'label': 'negative'},
+                      {'id': 3, 'review': 'Never see again :( horrible', 'prediction': 'positive',
+                       'probability': '50%', 'label': 'negative'},
+                      {'id': 4, 'review': 'Loved it!', 'prediction': 'positive',
+                          'probability': '98%', 'label': 'positive'}
+                      ]
 
 @app.route('/', methods=['POST','GET'])
 def newreview():
@@ -18,8 +26,10 @@ def newreview():
         X = vect.transform(review)
         prediction = label[clf.predict(X)[0]]
         probability = clf.predict_proba(X).max()*100
-        return render_template('result.html', review=review, prediction=prediction, probability=probability)
-    return render_template('new_review.html',form=form)
+        review_label = form.label.data
+        return render_template('result.html', review=review, prediction=prediction, probability=probability,
+        review_label=review_label)
+    return render_template('new_review.html',form=form,predictions=stored_predictions)
 
 
 if __name__ == "__main__":
--- forms.py
+++ forms.py
@@ -1,9 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField
+from wtforms import StringField, SubmitField, TextAreaField, RadioField
 from wtforms.validators import DataRequired
 
 
 class ReviewForm(FlaskForm):
     content = TextAreaField('Review', validators=[DataRequired()])
+    label = RadioField('Label', validators=[DataRequired()], choices=[
+                       ('positive', 'positive'), ('negative', 'negative')])
     submit = SubmitField('Predict')
 
